c30a2eec209b2dcc14b3d804173d526a98bae9ef
ba0061@naver.com <sweet.waiting0l@icloud.com>
2023-08-08 17:57:09 +0900

    feat: add map, fixed intersection observer

--- src/components/MainTexts.jsx
+++ src/components/MainTexts.jsx
@@ -6,9 +6,11 @@ const MainTexts = () => {
       entries.forEach(entry => {
         if(entry.isIntersecting) {
           if(entry.target.classList.contains('animate-left')) {
-            entry.target.classList.add('animate-left-active')
+            entry.target.classList.add('animate-left-active');
+            entry.target.classList.remove('opacity-0');
           } else if(entry.target.classList.contains('animate-right')) {
-            entry.target.classList.add('animate-right-active')
+            entry.target.classList.add('animate-right-active');
+            entry.target.classList.remove('opacity-0');
           }
 
           observer.unobserve(entry.target)
@@ -32,11 +34,11 @@ const MainTexts = () => {
 
   return(
     <div className="h-240 text-stone-950 relative">
-      <span className="absolute left-24 text-xl leading-snug animate-left">
+      <span className="absolute opacity-0 left-24  text-xl leading-snug animate-left">
         세계보건기구(WHO)에서는 건강을 <br /> <span className='text-3xl'>사회적 육체적 정신적</span><br />으로 완전히 안녕한 상태에 놓여있는 것이라고 정의합니다.
       </span>
 
-      <span className="absolute right-20 top-56 text-xl leading-snug animate-right">
+      <span className="absolute opacity-0 right-20 top-56 text-xl leading-snug animate-right">
         <span className="text-3xl"><span className="text-green-600">사</span><span className="text-purple-600">육</span><span className="text-red-600">신</span></span> 건강지킴이 연구소에서는 <br />
         건강의 안녕을 위해 상담 및 연구 하고 있습니다.<br /> <br />
 
@@ -45,7 +47,7 @@ const MainTexts = () => {
       </span>
 
 
-      <span className="absolute left-24 top-128 text-xl leading-snug animate-left">
+      <span className="absolute opacity-0 left-24 top-128 text-xl leading-snug animate-left">
         이전과 같은 방식으로는 더 이상 성장할 수 없습니다. <br />
         봉담은 새로운 시장과 기술을 창조하여 <br />
         더욱 더 성장할 것입니다. <br /> <br />
--- src/components/pages/WayCome.jsx
+++ src/components/pages/WayCome.jsx
@@ -4,25 +4,25 @@ import Navbar from "../Navbar";
 
 const WayCome = () => {
 
-  // const container = useRef(null);
+  const container = useRef(null);
   
-  // useEffect(() => {
-  //   const options = {
-  //     center: new window.kakao.maps.LatLng(37.222528, 126.9855495),
-  //     level: 3
-  //   };
-  //   const map = new window.kakao.maps.Map(container.current, options);
-  //   const markerPosition = new window.kakao.maps.LatLng(37.222528, 126.9855495);
-  //   const marker = new window.kakao.maps.Marker({
-  //     position: markerPosition
-  //   });
-  //   marker.setMap(map);
-  // }, []);
+  useEffect(() => {
+    const options = {
+      center: new window.kakao.maps.LatLng(37.222528, 126.9855495),
+      level: 3
+    };
+    const map = new window.kakao.maps.Map(container.current, options);
+    const markerPosition = new window.kakao.maps.LatLng(37.222528, 126.9855495);
+    const marker = new window.kakao.maps.Marker({
+      position: markerPosition
+    });
+    marker.setMap(map);
+  }, []);
 
   return (
     <div className="w-screen h-screen flex flex-col items-center">
       <Navbar />
-      {/* <div id='map' ref={container} className="w-5/6 h-5/6" /> */}
+      <div id='map' ref={container} className="w-5/6 h-5/6" />
       <Footer />
     </div>
   )
