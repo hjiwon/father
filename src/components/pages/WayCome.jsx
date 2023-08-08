20f48c260caffd496cc8a2e6b81996e898647f46
ba0061@naver.com <sweet.waiting0l@icloud.com>
2023-08-08 18:13:50 +0900

    fix: useEffect 방식 재변경

--- src/components/pages/WayCome.jsx
+++ src/components/pages/WayCome.jsx
@@ -3,20 +3,26 @@ import Footer from "../Footer";
 import Navbar from "../Navbar";
 
 const WayCome = () => {
-
   const container = useRef(null);
-  window.onload = function () {
-    const options = {
-      center: new window.kakao.maps.LatLng(37.222528, 126.9855495),
-      level: 3
+  
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      const options = {
+        center: new window.kakao.maps.LatLng(37.222528, 126.9855495),
+        level: 3
+      };
+      const map = new window.kakao.maps.Map(container.current, options);
+      const markerPosition = new window.kakao.maps.LatLng(37.222528, 126.9855495);
+      const marker = new window.kakao.maps.Marker({
+        position: markerPosition
+      });
+      marker.setMap(map);
+    }, 800); // 2초 (2000ms) 지연
+
+    return () => {
+      clearTimeout(timeoutId); // 컴포넌트가 언마운트될 때 타임아웃 클리어
     };
-    const map = new window.kakao.maps.Map(container.current, options);
-    const markerPosition = new window.kakao.maps.LatLng(37.222528, 126.9855495);
-    const marker = new window.kakao.maps.Marker({
-      position: markerPosition
-    });
-    marker.setMap(map);
-  }
+  }, []);
 
   return (
     <div className="w-screen h-screen flex flex-col items-center">
